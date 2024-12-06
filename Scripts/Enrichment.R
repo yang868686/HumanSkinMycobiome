9f3a2ef40e957c400b2e65cfc1c03498a6e1b06b
Ying Yang <86541281+yang868686@users.noreply.github.com>
2024-12-06 16:43:15 +0100

    Update Enrichment.R

--- Scripts/Enrichment.R
+++ Scripts/Enrichment.R
@@ -2,8 +2,8 @@ library(clusterProfiler)
 library(org.Hs.eg.db)
 library(ggplot2)
 
-OrgDb = "org.Hs.eg.db"  # "org.Mm.eg.db"  "org.Hs.eg.db"
-genetype = "ENSEMBL"    # "SYMBOL"   "ENSEMBL"
+OrgDb = "org.Hs.eg.db"  
+genetype = "ENSEMBL"   
 
 tmp <- bitr(gene,fromType = "SYMBOL", 
                    toType = "ENTREZID",
@@ -41,14 +41,13 @@ ego <- enrichGO(
 
 lineage1_ego <- clusterProfiler::simplify( 
   ego,
-  cutoff=0.7,  
   by="p.adjust",
   select_fun=min
 )
 
 dotp <- clusterProfiler::dotplot(lineage1_ego,
                                   size="GeneRatio",
-                                  showCategory=10, #10 显示的是padjust最小的前10个
+                                  showCategory=10, 
                                   label_format=20,
                                   font.size=8, 
 ) + scale_size_area(max_size = 10) +
